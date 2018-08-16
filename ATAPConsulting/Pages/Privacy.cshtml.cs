7c4be9ec501e5aae76ac36d356ae76d6296627b6
Bill Hertzing <bill.hertzing@gmail.com>
2018-08-15 22:45:51 -0400

    initial commit

--- ATAPConsulting/Pages/Privacy.cshtml.cs
+++ ATAPConsulting/Pages/Privacy.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebApplication1.Pages
+namespace ATAPWebSitesATAPConsulting.Pages
 {
     public class PrivacyModel : PageModel
     {
