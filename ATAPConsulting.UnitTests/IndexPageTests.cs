c6e4aca1352add01f88bba54fc8710863cfbcd5b
Bill Hertzing <bill.hertzing@gmail.com>
2018-08-16 12:35:23 -0400

    closes AddInitialUnitTest (#3)
    Added initial xUnitTest project, various dependencies, and first unit test.

---
+++ ATAPConsulting.UnitTests/IndexPageTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using Moq;
+
+namespace ATAPConsulting.UnitTests
+{
+    public class IndexPageTests
+    {
+        [Fact]
+        public void IndexPageTest1()
+        {
+      //Arrange
+
+      //Act
+
+      //Assert
+      // Quite useless at this point in time, since the web site is purely static.
+      Assert.True(true);
+    }
+    }
+}
