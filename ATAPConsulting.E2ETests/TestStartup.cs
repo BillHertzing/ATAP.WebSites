d4a7efe26a425e159b814e726e7e3ece7136db84
Bill Hertzing <bill.hertzing@gmail.com>
2018-08-27 13:04:04 -0400

    Initial project for E2E testing added

---
+++ ATAPConsulting.E2ETests/TestStartup.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+namespace ATAPConsulting.E2ETests
+{
+    public class TestStartup : ATAPWebSitesATAPConsulting.Startup
+  {
+    public TestStartup(IConfiguration configuration) : base(configuration)
+    {
+    }
+
+    public  void ConfigureDatabase(IServiceCollection services)
+    {
+
+    }
+
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+      // Perform all the configuration in the base class
+      base.Configure(app, env);
+      if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
